1b8b3c98d24d05e7d960607f8f1605c92ac21af2
Beat Walti <beat.walti@adesso.ch>
2019-12-18 17:54:24 +0100

    added comments, optimized some details

--- src/Hypomos.Web/wwwroot/tailwind/src/index.js
+++ src/Hypomos.Web/wwwroot/tailwind/src/index.js
@@ -4,14 +4,16 @@ import './styles.css';
 
 const Index = () => {
     return <div id="plex" className="flex flex-col absolute inset-0">
-        <div id="nav-bar" className="flex justify-between p-2 h-16 bg-gray-800 text-gray-200">
+        <div id="nav-bar" className="flex justify-between p-2 h-16 bg-gray-800 text-gray-400">
             <div className="flex items-center">
-                <button className="w-12 h-12 group hover:bg-gray-700">
+                {/* burger menu: */}
+                <button className="w-12 h-12 group hover:text-white">
                     <svg className="m-auto svg-defaults" >
                         <path d="M4 5h16a1 1 0 010 2H4a1 1 0 110-2zm0 6h16a1 1 0 010 2H4a1 1 0 010-2zm0 6h16a1 1 0 010 2H4a1 1 0 010-2z" />
                     </svg>
                 </button>
 
+                {/* title */}
                 <h1 className="tracking-wider font-semibold">Hypomos</h1>
 
                 <div className="flex w-88 mx-8 my-1 bg-gray-700 rounded-lg font-sans focus-within:bg-gray-200 focus-within:text-gray-700">
@@ -22,19 +24,23 @@ const Index = () => {
                 </div>
             </div>
 
+            {/* action items */}
             <div className="flex items-center">
+                {/* notifications */}
                 <button className="w-12 h-12 group hover:bg-gray-700">
                     <svg className="m-auto svg-defaults" >
                         <path className="heroicon-ui" d="M15 19a3 3 0 0 1-6 0H4a1 1 0 0 1 0-2h1v-6a7 7 0 0 1 4.02-6.34 3 3 0 0 1 5.96 0A7 7 0 0 1 19 11v6h1a1 1 0 0 1 0 2h-5zm-4 0a1 1 0 0 0 2 0h-2zm0-12.9A5 5 0 0 0 7 11v6h10v-6a5 5 0 0 0-4-4.9V5a1 1 0 0 0-2 0v1.1z" />
                     </svg>
                 </button>
 
+                {/* settings */}
                 <button className="w-12 h-12 group hover:bg-gray-700">
                     <svg className="m-auto svg-defaults" >
                         <path d="M9 4.58V4c0-1.1.9-2 2-2h2a2 2 0 012 2v.58a8 8 0 011.92 1.11l.5-.29a2 2 0 012.74.73l1 1.74a2 2 0 01-.73 2.73l-.5.29a8.06 8.06 0 010 2.22l.5.3a2 2 0 01.73 2.72l-1 1.74a2 2 0 01-2.73.73l-.5-.3A8 8 0 0115 19.43V20a2 2 0 01-2 2h-2a2 2 0 01-2-2v-.58a8 8 0 01-1.92-1.11l-.5.29a2 2 0 01-2.74-.73l-1-1.74a2 2 0 01.73-2.73l.5-.29a8.06 8.06 0 010-2.22l-.5-.3a2 2 0 01-.73-2.72l1-1.74a2 2 0 012.73-.73l.5.3A8 8 0 019 4.57zM7.88 7.64l-.54.51-1.77-1.02-1 1.74 1.76 1.01-.17.73a6.02 6.02 0 000 2.78l.17.73-1.76 1.01 1 1.74 1.77-1.02.54.51a6 6 0 002.4 1.4l.72.2V20h2v-2.04l.71-.2a6 6 0 002.41-1.4l.54-.51 1.77 1.02 1-1.74-1.76-1.01.17-.73a6.02 6.02 0 000-2.78l-.17-.73 1.76-1.01-1-1.74-1.77 1.02-.54-.51a6 6 0 00-2.4-1.4l-.72-.2V4h-2v2.04l-.71.2a6 6 0 00-2.41 1.4zM12 16a4 4 0 110-8 4 4 0 010 8zm0-2a2 2 0 100-4 2 2 0 000 4z" />
                     </svg>
                 </button>
 
+                {/* user */}
                 <button className="h-12 px-2 w-auto group hover:bg-gray-700 text-left">
                     <svg className="inline mr-2 svg-defaults" >
                         <path d="M12 12a5 5 0 110-10 5 5 0 010 10zm0-2a3 3 0 100-6 3 3 0 000 6zm9 11a1 1 0 01-2 0v-2a3 3 0 00-3-3H8a3 3 0 00-3 3v2a1 1 0 01-2 0v-2a5 5 0 015-5h8a5 5 0 015 5v2z" />
@@ -45,10 +51,11 @@ const Index = () => {
         </div>
 
         <div id="content" className="flex-grow relative overflow-x-hidden overflow-y-auto">
-            <div className="flex flex-row absolute inset-0 overflow-hidden bg-gray-800 text-gray-300">
+            <div className="flex flex-row absolute inset-0 overflow-hidden bg-gray-800 text-gray-400">
                 <div className="flex flex-col">
                     <div className="flex-grow w-64 overflow-y-auto dark-scrollbar" >
                         <nav>
+                            {/* fotos */}
                             <div className="flex h-12 selected group hover:bg-gray-700 hover:text-gray-100">
                                 <a className="flex flex-grow flex-shrink pl-4 items-center">
                                     <div className="">
@@ -61,8 +68,35 @@ const Index = () => {
                                     </div>
                                 </a>
                             </div>
-                            <div className="h-12">Alben</div>
-                            <div className="h-12">Cleaning</div>
+
+                            {/* alben */}
+                            <div className="flex h-12 group hover:bg-gray-700 hover:text-gray-100">
+                                <a className="flex flex-grow flex-shrink pl-4 items-center">
+                                    <div className="">
+                                        <svg className="svg-defaults inline mr-2">
+                                            <path class="heroicon-ui" d="M5 3h4a2 2 0 012 2v4a2 2 0 01-2 2H5a2 2 0 01-2-2V5c0-1.1.9-2 2-2zm0 2v4h4V5H5zm10-2h4a2 2 0 012 2v4a2 2 0 01-2 2h-4a2 2 0 01-2-2V5c0-1.1.9-2 2-2zm0 2v4h4V5h-4zM5 13h4a2 2 0 012 2v4a2 2 0 01-2 2H5a2 2 0 01-2-2v-4c0-1.1.9-2 2-2zm0 2v4h4v-4H5zm10-2h4a2 2 0 012 2v4a2 2 0 01-2 2h-4a2 2 0 01-2-2v-4c0-1.1.9-2 2-2zm0 2v4h4v-4h-4z" />
+                                        </svg>
+                                    </div>
+                                    <div className="pl-2">
+                                        <span>Alben</span>
+                                    </div>
+                                </a>
+                            </div>
+
+                            {/* cleaning */}
+                            <div className="flex h-12 group hover:bg-gray-700 hover:text-gray-100">
+                                <a className="flex flex-grow flex-shrink pl-4 items-center">
+                                    <div className="">
+
+                                        <svg className="svg-defaults inline mr-2">
+                                            <path class="heroicon-ui" d="M20 9v10a2 2 0 01-2 2H6a2 2 0 01-2-2V9a2 2 0 01-2-2V5c0-1.1.9-2 2-2h16a2 2 0 012 2v2a2 2 0 01-2 2zm0-2V5H4v2h16zM6 9v10h12V9H6zm4 2h4a1 1 0 010 2h-4a1 1 0 010-2z" />
+                                        </svg>
+                                    </div>
+                                    <div className="pl-2">
+                                        <span>Cleaning</span>
+                                    </div>
+                                </a>
+                            </div>
                         </nav>
                     </div>
                 </div>
