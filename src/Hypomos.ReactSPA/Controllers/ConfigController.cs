15de0a20f3d1bb67658d401cb8efe35ea41af9c2
Beat Walti <BWalti@users.noreply.github.com>
2020-05-02 23:10:54 +0200

    added a config controller
    - added redux

---
+++ src/Hypomos.ReactSPA/Controllers/ConfigController.cs
@@ -0,0 +1,23 @@
+﻿namespace Hypomos.ReactSPA.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ConfigController : ControllerBase
+    {
+        [HttpGet]
+        public AppConfiguration Get()
+        {
+            return new AppConfiguration
+            {
+                SomeSetting = "Hello World!"
+            };
+        }
+    }
+
+    public class AppConfiguration
+    {
+        public string SomeSetting { get; set; }
+    }
+}
