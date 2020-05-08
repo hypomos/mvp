fb55be3f04308e0a8acb04082ca0c3418108ad66
Beat Walti <BWalti@users.noreply.github.com>
2020-05-08 22:57:30 +0200

    replaced navigation labels and icons

--- src/Hypomos.ReactSPA/ClientApp/src/features/app/components/Navigation.tsx
+++ src/Hypomos.ReactSPA/ClientApp/src/features/app/components/Navigation.tsx
@@ -1,29 +1,37 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-class Navigation extends React.Component {
+import { RootState } from 'MyTypes';
+
+class Navigation extends React.Component<Props> {
     render() {
+        const { pathname } = this.props;
+        if(pathname){
+            debugger;
+        }
+
         return (
             <div className="shadow-lg h-16 fixed bottom-0 mt-12 md:relative md:h-screen z-10 w-full md:w-48">
                 <div className="md:mt-12 md:w-48 md:fixed md:left-0 md:top-0 content-center md:content-start text-left justify-between">
                     <ul className="list-reset flex flex-row md:flex-col py-0 md:py-3 px-1 md:px-2 text-center md:text-left">
                         <li className="mr-3 flex-1">
                             <a href="#" className="block py-1 md:py-3 pl-1 align-middle text-white no-underline hover:text-white border-b-2 border-gray-800 hover:border-pink-500">
-                                <i className="fas fa-tasks pr-0 md:pr-3"></i><span className="pb-1 md:pb-0 text-xs md:text-base text-gray-600 md:text-gray-400 block md:inline-block">Tasks</span>
+                                <i className="fas fa-home pr-0 md:pr-3"></i><span className="pb-1 md:pb-0 text-xs md:text-base text-gray-600 md:text-gray-400 block md:inline-block">Home</span>
                             </a>
                         </li>
                         <li className="mr-3 flex-1">
                             <a href="#" className="block py-1 md:py-3 pl-1 align-middle text-white no-underline hover:text-white border-b-2 border-gray-800 hover:border-purple-500">
-                                <i className="fa fa-envelope pr-0 md:pr-3"></i><span className="pb-1 md:pb-0 text-xs md:text-base text-gray-600 md:text-gray-400 block md:inline-block">Messages</span>
+                                <i className="fas fa-photo-video pr-0 md:pr-3"></i><span className="pb-1 md:pb-0 text-xs md:text-base text-gray-600 md:text-gray-400 block md:inline-block">Items</span>
                             </a>
                         </li>
                         <li className="mr-3 flex-1">
                             <a href="#" className="block py-1 md:py-3 pl-1 align-middle text-white no-underline hover:text-white border-b-2 border-blue-600">
-                                <i className="fas fa-chart-area pr-0 md:pr-3 text-blue-600"></i><span className="pb-1 md:pb-0 text-xs md:text-base text-white md:text-white block md:inline-block">Analytics</span>
+                                <i className="fas fa-book-open pr-0 md:pr-3 text-blue-600"></i><span className="pb-1 md:pb-0 text-xs md:text-base text-white md:text-white block md:inline-block">Collections</span>
                             </a>
                         </li>
                         <li className="mr-3 flex-1">
                             <a href="#" className="block py-1 md:py-3 pl-0 md:pl-1 align-middle text-white no-underline hover:text-white border-b-2 border-gray-800 hover:border-red-500">
-                                <i className="fa fa-wallet pr-0 md:pr-3"></i><span className="pb-1 md:pb-0 text-xs md:text-base text-gray-600 md:text-gray-400 block md:inline-block">Payments</span>
+                                <i className="fa fa-recycle pr-0 md:pr-3"></i><span className="pb-1 md:pb-0 text-xs md:text-base text-gray-600 md:text-gray-400 block md:inline-block">Cleaning</span>
                             </a>
                         </li>
                     </ul>
@@ -33,4 +41,13 @@ class Navigation extends React.Component {
     }
 }
 
-export default Navigation;
+const mapStateToProps = (state: RootState) => ({
+    pathname: state.router.location.pathname,
+    search: state.router.location.search,
+    hash: state.router.location.hash,
+});
+
+
+type Props = ReturnType<typeof mapStateToProps>;
+
+export default connect(mapStateToProps)(Navigation);
