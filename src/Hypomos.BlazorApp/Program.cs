9c4c29f93dc453b8d5fbf744068e19c3d04fe408
Beat Walti <BWalti@users.noreply.github.com>
2020-10-19 10:14:08 +0200

    small changes

--- src/Hypomos.BlazorApp/Program.cs
+++ src/Hypomos.BlazorApp/Program.cs
@@ -38,10 +38,8 @@ namespace Hypomos.BlazorApp
                 .ConfigureAppConfiguration(
                     (context, builder) =>
                     {
-
-                        builder.AddJsonFile("appsettings.json")
-                                .AddEnvironmentVariables("HYPOMOS_")
-                                .AddKubernetes(loggerFactory: GetLoggerFactory());
+                        builder.AddEnvironmentVariables("HYPOMOS_");
+                        builder.AddKubernetes(loggerFactory: GetLoggerFactory());
 
                         Console.WriteLine(Platform.IsKubernetes ? "!!! WE ARE ON K8S !!!" : "!!! WE ARE ---NOT--- ON K8S !!!");
                     })
